26fc1a303b84e6f60f3623757779630b2c690707
Wagner Xu <wagnerhsu@hotmail.com>
2022-09-13 09:23:07 +0800

    update files

--- chapter14/CoolWeather/app/src/main/java/com/coolweather/android/ChooseAreaFragment.java
+++ chapter14/CoolWeather/app/src/main/java/com/coolweather/android/ChooseAreaFragment.java
@@ -3,8 +3,7 @@ package com.coolweather.android;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.util.Log;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,6 +14,7 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.fragment.app.Fragment;
 import com.coolweather.android.db.City;
 import com.coolweather.android.db.County;
 import com.coolweather.android.db.Province;
