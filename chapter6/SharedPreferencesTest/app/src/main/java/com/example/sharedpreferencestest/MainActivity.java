8946ccc15fe106d63d194d7e5a509b3f11dcb748
Wagner Xu <wagnerhsu@hotmail.com>
2022-09-17 23:13:48 +0800

    update files

--- chapter6/SharedPreferencesTest/app/src/main/java/com/example/sharedpreferencestest/MainActivity.java
+++ chapter6/SharedPreferencesTest/app/src/main/java/com/example/sharedpreferencestest/MainActivity.java
@@ -1,11 +1,11 @@
 package com.example.sharedpreferencestest;
 
 import android.content.SharedPreferences;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
